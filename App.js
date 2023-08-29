974b575c7274ed584ec7b25741e163bc18c3bc83
ErickLao123 <105694432+ErickLao123@users.noreply.github.com>
2023-08-30 11:38:24 +1200

    Login?

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
+
+  const Login = () => {
+    // Replace these with your preset username and password
+    const setUser = 'PortfolioPal';
+    const setPass = 'AIVestor';
+
+    if (user === setUser && pass === setPass) {
+      // Successful login
+      Alert.alert('Login Successful', 'Welcome back, PortfolioPal!');
+    } else {
+      // Failed login
+      Alert.alert('Login Failed', 'Invalid username or password.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>No Jack Yuhwan!</Text>
-      <Text>Jack Yuhwan!</Text>
+      <Text> Welcome Back!</Text>
+      <Text> Please Login!</Text>
+      <TextInput
+        placeholder="Username"
+        onChangeText={text => setUser(text)}
+        value={user}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        onChangeText={text => setPass(text)}
+        value={pass}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Login" onPress={Login} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +49,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginVertical: 10,
+    paddingHorizontal: 10,
   },
 });

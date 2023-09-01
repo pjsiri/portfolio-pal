221a08147cf20531f392773a26566f0c5b505c69
Richard Hu <rh20020421@gmail.com>
2023-09-01 15:21:46 +1200

    Set up basic home page structure

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,25 +1,78 @@
 import React, { useState } from 'react';
-import { Text, View, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput } from 'react-native';
 
 const HomePage = () => {
     return (
-        <View>
-            <Text>PortfolioPal</Text>
-            <Text>Trending Stocks</Text>
-            <Text>Newest Stocks</Text>
+        <View style={styles.appContainer}>
+          
+          <View>
+            <Image style={styles.appLogo} source={require('../assets/app_logo.png')} />
+          </View>
+
+          <ScrollView>
+            <Text style={styles.title}>PortfolioPal</Text>
+
+            <View style={styles.componentContainer}>
+              <Text style={styles.subtitle}>Trending Stocks</Text>
+              <View style={styles.trendingStocks}>
+                
+              </View>
+            </View>
+
+            <View style={styles.componentContainer}>
+              <Text style={styles.subtitle}>Newest Stocks</Text>
+              <View style={styles.newestStocks}>
+                
+              </View>
+            </View>
+          </ScrollView>
+
         </View>
     )
 }
 
-const styles = {
-    input: {
-      width: 200,
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      marginVertical: 10,
-      paddingHorizontal: 10,
+const styles = StyleSheet.create({
+    appContainer: {
+      flex: 1,
+      padding: 50,
+      paddingLeft: 20,
+      paddingRight: 20,
+      width: '100%',
+      backgroundColor: 'grey',
     },
-  };
+
+    componentContainer: {
+      //backgroundColor: 'black',
+    },
+
+    trendingStocks: {
+      height: 250,
+      backgroundColor: 'white',
+    },
+
+    newestStocks: {
+      height: 600,
+      backgroundColor: 'white',
+    },
+
+    title: {
+      fontWeight: 'bold',
+      fontSize: 30,
+      paddingTop: 20,
+      paddingBottom: 20,
+    },
+
+    subtitle: {
+      fontWeight: 'bold',
+      fontSize: 20,
+      padding: 10,
+    },
+
+    appLogo: {
+      width: 60,
+      height: 60,
+      
+    },
+  });
 
 export default HomePage

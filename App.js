707b58f6e08f9a510fe04cf8bae3b58399c7f566
pjsiri <goinmc@gmail.com>
2023-09-03 01:16:11 +1200

    Recentre the text area in login screen.

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import HomePage from './src/home-page/HomePage.js';
 export default function App() {
   return (
     <View style={styles.container}>
-      <RegisterScreen />
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );

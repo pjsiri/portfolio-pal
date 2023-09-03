d78f6d7891fd61939f3f415ac7ac7883402c324f
yhkim8046 <82135063+yhkim8046@users.noreply.github.com>
2023-09-03 15:52:35 +1200

    Calculation function edited with adding coloring font

--- src/PortFolio.js
+++ src/PortFolio.js
@@ -1,6 +1,32 @@
-let originalTotalPrice; 
-let changedTotalPrice; 
+import React, { useState } from 'react';
+import { Text, View, TextInput, Button, Alert } from 'react-native';
+import { Image } from 'react-native';
 
-const result = (originalTotalPrice,changedTotalPrice) =>{
-    return originalTotalPrice - changedTotalPrice;
+const portFolio = () => {
+    let originalTotalPrice = 10;
+    let changedTotalPrice = 9.6;
+    const result = (originalTotalPrice, changedTotalPrice) => {
+        return changedTotalPrice - originalTotalPrice;
+    }
+    const resultValue = result(originalTotalPrice, changedTotalPrice);
+    let moneyText = null;
+    if (resultValue > 0) {
+        moneyText = <Text style={styles.earnedMoney}>result: +{resultValue}</Text>;
+    } else {
+        moneyText = <Text style={styles.losesMoney}>result: {resultValue}</Text>;
+    }
+
+    let percentage = (resultValue / originalTotalPrice) * 100;
+
+    return (
+        <View style={styles.container}>
+            <Text>Original money: {originalTotalPrice}</Text>
+            <Text>Changed money: {changedTotalPrice}</Text>
+            {moneyText}
+            <Text>{percentage}%</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
+
+export default portFolio;

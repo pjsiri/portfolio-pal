3d50eabe94e2b7d687a9734b4053ddd90fe9d7d8
Richard Hu <rh20020421@gmail.com>
2023-09-08 18:15:15 +1200

    Fix empty uri bug

--- src/home-page/cards/trendingStockCard.jsx
+++ src/home-page/cards/trendingStockCard.jsx
@@ -9,17 +9,21 @@ const TrendingStockCard = ({ item, selectedStock, handleCardPress }) => {
   const router = useRouter();
   const { data, isLoading, error } = getLogo(item.symbol);
 
+  const isDataAvailable = data !== "";
+
   return (
     <TouchableOpacity
       style={styles.container(selectedStock, item)}
       onPress={() => handleCardPress(item)}
     >
       <TouchableOpacity style={styles.logoContainer(selectedStock, item)}>
-        <Image
-          source={{ uri: data }}
-          resizeMode="contain"
-          style={styles.logoImage}
-        />
+        {isDataAvailable && (
+          <Image
+            source={{ uri: data }}
+            resizeMode="contain"
+            style={styles.logoImage}
+          />
+        )}
       </TouchableOpacity>
     </TouchableOpacity>
   );

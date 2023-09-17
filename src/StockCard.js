ed564b97b9b9e342077495c7b1c659355c7822af
yhkim8046 <82135063+yhkim8046@users.noreply.github.com>
2023-09-17 20:17:52 +1200

    Conflict solved

--- src/StockCard.js
+++ src/StockCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet } from "react-native";
 import { View, Text, Image, TouchableOpacity } from 'react-native';
-import styles from "./home-page/cards/Cards.style";
+import styles from "./common/cards/Cards.style";
 import { COLORS, FONT, SHADOWS, SIZES } from "../constants/theme";
 
 const StockCard = ({ name, price, color }) => {

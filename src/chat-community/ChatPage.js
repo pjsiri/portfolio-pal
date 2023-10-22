ee2615f40de2dcca27d34dd21e0e8777d88a205a
pjsiri <goinmc@gmail.com>
2023-10-23 02:41:49 +1300

    Improved chatting community UI + avatar

--- src/chat-community/ChatPage.js
+++ src/chat-community/ChatPage.js
@@ -5,27 +5,41 @@ import {
   TextInput,
   TouchableOpacity,
   FlatList,
-  StyleSheet,
+  Image,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { getFirestore, collection, addDoc, serverTimestamp, onSnapshot } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import styles from "./ChatPage.style";
 
 const ChatPage = () => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const navigation = useNavigation();
+  const [userProfileImage, setUserProfileImage] = useState(null);
   const firestore = getFirestore();
   const messagesRef = collection(firestore, "messages");
   const auth = getAuth();
   const user = auth.currentUser;
 
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // Set the user's profile image
+      if (user.photoURL) {
+        setUserProfileImage(user.photoURL);
+      }
+      else
+      {
+        setUserProfileImage("https://github.com/ErickLao123/2023-S2-51-AIVestor/raw/main/assets/default_profile.png");
+      }
+    }
+  });
+
   const sendMessage = async () => {
     if (message.trim() !== "") {
       await addDoc(messagesRef, {
         content: message,
-        sender: user.uid, // Replace with actual sender ID
+        sender: user.displayName, // Username for the user.
         timestamp: serverTimestamp(),
       });
       setMessage("");
@@ -33,13 +47,15 @@ const ChatPage = () => {
   };
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(messagesRef, (querySnapshot) => {
-      const updatedMessages = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setMessages(updatedMessages);
-    });
+    const unsubscribe = onSnapshot(
+      collection(firestore, "messages"),
+      (querySnapshot) => {
+        const updatedMessages = querySnapshot.docs
+          .map((doc) => ({ id: doc.id, ...doc.data() }))
+          .sort((a, b) => b.timestamp - a.timestamp); // Sort by timestamp in descending order
+        setMessages(updatedMessages);
+      }
+    );
 
     return () => unsubscribe();
   }, []);
@@ -50,9 +66,20 @@ const ChatPage = () => {
         data={messages}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View style={styles.messageContainer}>
-            <Text style={styles.sender}>{item.sender}:</Text>
-            <Text style={styles.message}>{item.content}</Text>
+          <View style={styles.cardContainer}>
+            <View style={styles.messageContainer}>
+              <Image
+                source= {{url: userProfileImage}} 
+                style={styles.avatar}
+              />
+              <View style={styles.messageContent}>
+                <Text style={styles.sender}>{item.sender}:</Text>
+                <Text style={styles.message}>{item.content}</Text>
+                <Text style={styles.timestamp}>
+                  {new Date(item.timestamp.toDate()).toLocaleString()}
+                </Text>
+              </View>
+            </View>
           </View>
         )}
       />

7853f58f47bfcb5dc7fa518e1fec29028bc10144
JasonAlessandro <105477409+JasonAlessandro@users.noreply.github.com>
2023-10-22 19:12:49 +1300

    Commit bugfix

--- src/settings-page/SettingsPage.js
+++ src/settings-page/SettingsPage.js
@@ -1,34 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Switch, StyleSheet, TouchableOpacity, Alert, Image } from "react-native";
+import {
+  View,
+  Text,
+  Switch,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  Image,
+  ScrollView,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Picker } from "@react-native-picker/picker";
 import { useDarkMode } from "../common/darkmode/DarkModeContext";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useIsFocused } from "@react-navigation/native";
 
 const SettingsPage = () => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState("");
+  const [userProfileImage, setUserProfileImage] = useState(null);
   const navigation = useNavigation();
-  /* const [selectedCurrency, setSelectedCurrency] = useState("NZD"); */
-
+  const auth = getAuth();
+  const isFocused = useIsFocused();
   useEffect(() => {
-    const auth = getAuth();
+    if (isFocused) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          if (user.displayName) {
+            setUsername(user.displayName);
+          }
 
-    // Listen for changes in the user's authentication state
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // The user is signed in, and you can access the user's information, including the display name
-        if (user.displayName) {
-          setUsername(user.displayName);
+          // Set the user's profile image
+          if (user.photoURL) {
+            setUserProfileImage(user.photoURL);
+          }
         }
-        if (user.email) {
-          setEmail(user.email);
-        }
-      }
-    });
-  }, []);
-
+      });
+    }
+  }, [isFocused]);
   const handleLogout = () => {
     Alert.alert(
       "Confirm Logout",
@@ -41,8 +50,11 @@ const SettingsPage = () => {
         {
           text: "Logout",
           onPress: () => {
-            // Perform logout action here
             navigation.navigate("Login");
+            navigation.reset({
+              index: 0,
+              routes: [{ name: "Login" }],
+            });
           },
         },
       ],
@@ -50,46 +62,44 @@ const SettingsPage = () => {
     );
   };
 
-  const handleEducationNavigation = () => {
-    navigation.navigate("Education");
-  };
-
-  const handleProfileNavigation = () => {
-    navigation.navigate("Profile");
-  };
-
-    const handleChatNavigation = () => {
-    navigation.navigate("Chat");
-  };
-
   return (
-    <View style={[styles.container, isDarkMode && styles.darkModeContainer]}>
-      <Text style={[styles.title, isDarkMode && styles.darkModeText]}>
-        Profile
-      </Text>
-      <TouchableOpacity onPress={handleProfileNavigation}>
-        <View style={[styles.profileContainer, isDarkMode && styles.darkModeContainer]}>
-          <View style={styles.profileImageContainer}>
-            <Image
-              source={{
-                uri:
-                  "https://github.com/ErickLao123/2023-S2-51-AIVestor/raw/main/assets/default_profile.png",
-              }}
-              style={styles.profileImage}
-            />
-          </View>
-          <Text style={[styles.usernameText, isDarkMode && styles.darkModeText]}>Username: {username}</Text>
-          <Text style={[styles.usernameText, isDarkMode && styles.darkModeText]}>Email: {email}</Text>
+    <ScrollView contentContainerStyle={[styles.container, isDarkMode && styles.darkModeContainer]}>
+      <Text style={[styles.title, isDarkMode && styles.darkModeText]}>Profile</Text>
+      <View style={styles.profileContainer}>
+        <View style={styles.profileImageBorder}>
+          <Image
+            source={{
+              uri: userProfileImage ||
+                "https://github.com/ErickLao123/2023-S2-51-AIVestor/raw/main/assets/default_profile.png",
+            }}
+            style={styles.profileImage}
+          />
         </View>
+        <Text style={[styles.usernameText, isDarkMode && styles.darkModeText]}>{username}</Text>
+        <TouchableOpacity style={styles.editProfileButton} onPress={() => navigation.navigate("Profile")}>
+          <Text style={styles.editProfileText}>Edit profile</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={[styles.title, isDarkMode && styles.darkModeText]}>Security</Text>
+      <TouchableOpacity style={styles.optionBubble} onPress={() => navigation.navigate("ChangePassword")}>
+        <Image
+          source={{
+            uri: "https://github.com/ErickLao123/2023-S2-51-AIVestor/raw/main/assets/password.png",
+          }}
+          style={styles.bubbleIcon}
+        />
+        <Text style={[styles.bubbleText, isDarkMode && styles.darkModeText]}>Change Password</Text>
       </TouchableOpacity>
-      <Text style={[styles.title, isDarkMode && styles.darkModeText]}>
-        Appearance
-      </Text>
-      <View style={[styles.appearanceContainer, isDarkMode && styles.darkModeContainer]}>
-        <View style={styles.setting}>
-          <Text style={[styles.settingText, isDarkMode && styles.darkModeText]}>
-            Dark Mode
-          </Text>
+      <Text style={[styles.title, isDarkMode && styles.darkModeText]}>Appearance</Text>
+      <View style={styles.optionBubble}>
+        <Image
+          source={{
+            uri: "https://github.com/ErickLao123/2023-S2-51-AIVestor/raw/main/assets/dark-mode-icon.png",
+          }}
+          style={styles.bubbleIcon}
+        />
+        <TouchableOpacity onPress={toggleDarkMode} style={styles.toggleButton}>
+          <Text style={[styles.bubbleText, isDarkMode && styles.darkModeText]}>Dark Mode</Text>
           <Switch
             value={isDarkMode}
             onValueChange={toggleDarkMode}
@@ -97,100 +107,58 @@ const SettingsPage = () => {
             thumbColor={isDarkMode ? "#f5dd4b" : "#f4f3f4"}
             ios_backgroundColor="#3e3e3e"
           />
-        </View>
+        </TouchableOpacity>
       </View>
-      <Text style={[styles.title, isDarkMode && styles.darkModeText]}>
-        Tutorials
-      </Text>
-      <View style={[styles.appearanceContainer, isDarkMode && styles.darkModeContainer]}>
-      <TouchableOpacity onPress={handleEducationNavigation}>
-        <Text style={[styles.educationLink, isDarkMode && styles.darkModeText]}>
-          Educational Videos
-        </Text>
+      <Text style={[styles.title, isDarkMode && styles.darkModeText]}>Tutorials</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Education")}>
+        <View style={styles.optionBubble}>
+          <Image
+            source={{
+              uri: "https://github.com/ErickLao123/2023-S2-51-AIVestor/raw/main/assets/Video_icon.png",
+            }}
+            style={styles.bubbleIcon}
+          />
+          <Text style={[styles.bubbleText, isDarkMode && styles.darkModeText]}>Educational Videos</Text>
+        </View>
       </TouchableOpacity>
-      {}
-      <TouchableOpacity onPress={handleChatNavigation}>
-        <Text style={[styles.educationLink, isDarkMode && styles.darkModeText]}>
-          AIVestor
-        </Text>
+      <Text style={[styles.title, isDarkMode && styles.darkModeText]}>Support</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+        <View style={styles.optionBubble}>
+          <Image
+            source={{
+              uri: "https://github.com/ErickLao123/2023-S2-51-AIVestor/raw/main/assets/Bot_icon.png",
+            }}
+            style={styles.bubbleIcon}
+          />
+          <Text style={[styles.bubbleText, isDarkMode && styles.darkModeText]}>AIVestor Chat Bot</Text>
+        </View>
       </TouchableOpacity>
-    </View>
-    <View style={{ marginTop: 30 }}>
       <TouchableOpacity onPress={handleLogout}>
-        <Text style={styles.logoutButton}>
-          Logout
-        </Text>
+        <View style={styles.logoutButton}>
+          <Text style={styles.logoutText}>Logout</Text>
+        </View>
       </TouchableOpacity>
-    </View>
-  </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
+    marginTop: 30,
     alignItems: "center",
+    paddingBottom: 50,
   },
   profileContainer: {
     alignItems: "center",
     width: 350,
-    height: 250,
+    height: 280,
     borderColor: "gray",
     borderWidth: 1,
     paddingHorizontal: 10,
-    marginBottom: 5,
     borderRadius: 20,
   },
-  appearanceContainer: {
-    alignItems: "center",
-    width: 350,
-    height: 50,
-    borderColor: "gray",
-    borderWidth: 1,
-    paddingHorizontal: 10,
-    marginBottom: 5,
-    borderRadius: 20,
-  },
-  darkModeContainer: {
-    backgroundColor: "#333",
-  },
-  title: {
-    fontSize: 20,
-    marginTop: 20,
-    marginBottom: 10,
-    marginLeft: 30,
-    alignSelf: "flex-start",
-    fontWeight: "bold",
-  },
-  darkModeText: {
-    color: "#fff",
-  },
-  setting: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: 200,
-  },
-  settingText: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  logoutButton: {
-    marginTop: 50,
-    fontSize: 18,
-    color: "red",
-    textDecorationLine: "underline",
-  },
-  educationLink: {
-    fontSize: 18,
-    marginVertical: 10,
-    textDecorationLine: "underline",
-  },
-  usernameText: {
-    fontSize: 18,
-    marginLeft: 10,
-  },
-  profileImageContainer: {
+  profileImageBorder: {
     width: 120,
     height: 120,
     borderRadius: 60,
@@ -203,6 +171,75 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
+  editProfileButton: {
+    backgroundColor: "black",
+    padding: 10,
+    borderRadius: 20,
+    marginTop: 20,
+    width: "90%",
+    alignItems: "center",
+  },
+  editProfileText: {
+    color: "white",
+    fontSize: 16,
+  },
+  usernameText: {
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 20,
+    marginTop: 15,
+    marginBottom: 10,
+    marginLeft: 30,
+    alignSelf: "flex-start",
+    fontWeight: "bold",
+  },
+  darkModeContainer: {
+    backgroundColor: "#333",
+  },
+  darkModeText: {
+    color: "#fff",
+  },
+  toggleButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: 285,
+  },
+  logoutButton: {
+    width: 150,
+    height: 50,
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    marginTop: 25,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logoutText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  optionBubble: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: 350,
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginBottom: 5,
+    borderRadius: 20,
+  },
+  bubbleIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 8,
+  },
+  bubbleText: {
+    fontSize: 16,
+    marginLeft: 10,
+  },
 });
-
 export default SettingsPage;
